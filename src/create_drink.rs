879394c3b69251612808685720ede219245e40fe
ctroy978 <ctroy978@gmail.com>
2022-08-16 09:36:43 -0700

    basic create_ingredient added

---
+++ src/create_drink.rs
@@ -0,0 +1,76 @@
+use std::ops::Add;
+
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
+use yew::prelude::*;
+
+use crate::create_ingredient::CreateIngredient;
+use crate::text_box::TextBox;
+
+#[derive(Debug, Clone, Deserialize, Serialize, Default)]
+struct AddIngredients {
+    label: String,
+    unit: String,
+    qty: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, Default)]
+struct BuildDrink {
+    pub title: String,
+    pub rank: String,
+    pub directions: String,
+    pub add_ingredient: Vec<AddIngredients>,
+}
+
+#[function_component(CreateDrink)]
+pub fn create_drink() -> Html {
+    let state = use_state(|| BuildDrink::default());
+
+    //handle title
+    let cloned_state = state.clone();
+    let handle_title = Callback::from(move |value| {
+        let mut data = (*cloned_state).clone();
+        data.title = value;
+        cloned_state.set(data);
+    });
+
+    //handle rank
+    let cloned_state = state.clone();
+    let handle_rank = Callback::from(move |value| {
+        let mut data = (*cloned_state).clone();
+        data.rank = value;
+        cloned_state.set(data);
+    });
+
+    //handle directions
+    let cloned_state = state.clone();
+    let handle_dir = Callback::from(move |value| {
+        let mut data = (*cloned_state).clone();
+        data.directions = value;
+        cloned_state.set(data);
+    });
+
+    let cloned_state = state.clone();
+    let handle_ingredient = Callback::from(move |value: String| {
+        let mut data = (*cloned_state).clone();
+        let add_ingredients: AddIngredients = serde_json::from_str(&value.as_str()).unwrap();
+        data.add_ingredient.push(add_ingredients);
+        cloned_state.set(data);
+    });
+
+    html! {
+        <>
+        <h1>{"drink comp"}</h1>
+
+        <TextBox name = "title"  place_holder = {"Drink's name?"} handle_onchange = {handle_title}/>
+        <TextBox name = "rank"  place_holder = {"Drink's rank?"} handle_onchange = {handle_rank}/>
+        <TextBox name = "directions"  place_holder = {"Step by step directions?"} handle_onchange = {handle_dir}/>
+        <br />
+        <CreateIngredient handle_onclick = {handle_ingredient}/>
+
+        <p>{&state.title}</p>
+
+
+        </>
+    }
+}
